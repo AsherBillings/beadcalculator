63b5d63f686d1c598b70efc3eb603bf3ca44aab6
Asher Billings <262520482+AsherBillings@users.noreply.github.com>
2026-08-30 13:29:46 +1000

    Add csv file check

--- sum_beads_from_csv.py
+++ sum_beads_from_csv.py
@@ -7,6 +7,11 @@ if not sys.argv[1:]:
     print("Please provide csv file(s) as an argument.")
     sys.exit(1)
 
+for x in sys.argv[1:]:
+    if not x.endswith(".csv"):
+        print("File is not a csv file.")
+        sys.exit(1)
+
 for a in sys.argv[1:]:
     with open(a, 'r') as f:
         for x in f:
